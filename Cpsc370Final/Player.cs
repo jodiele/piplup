e110d4ed50bb21708bbc622bbb022ce6071c214d
ivannnito <idombiak@gmail.com>
2025-01-29 13:48:07 -0800

    Just added initial player class, setting username and the beginning console messages for when the game starts; just wanted to get some work done before I have to go to campus and won't be able to work :)

---
+++ Cpsc370Final/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace Cpsc370Final;
+
+public class Player
+{
+    private int moneyAmount;
+    private string _username;
+    public string Username { get => _username; set => _username = value; }
+    
+    
+
+}
--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -9,6 +9,23 @@ class Program
         
         // you can delete this if/when you like
         ShowArguments(args);
+        
+        Player user = new Player();
+        
+        // Guessing this will be where initializing the game will go
+        // as a placeholder putting down the String prompt of what the game is,
+        // how to play
+        Console.WriteLine("Welcome to our game [GAME NAME]!");
+        Console.WriteLine("What is your name?");
+        // will be passed back to user class 
+        String userName = Console.ReadLine();
+        // just setting username :) 
+        user.Username = userName;
+        
+        Console.WriteLine("This game is much like Wheel of Fortune, that said, how many rounds will you like to play?");
+        // thinking we could have a rounds class here which will be our actual game flow, will run until the rounds are over
+        int rounds = Convert.ToInt32(Console.ReadLine()); 
+        
     }
 
     // this is just an example of how to get the command
