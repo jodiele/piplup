ed6ecbb4343d284161a0ac42e726e6f62c4f8e11
Iván Dombiak <idombiak@gmail.com>
2025-01-29 20:23:48 -0800

    Added some more functionality with both player classes.

--- Cpsc370Final/AIPlayers.cs
+++ Cpsc370Final/AIPlayers.cs
@@ -5,13 +5,22 @@ public class AIPlayers {
 	private string[] aiNames = { "Tanner", "Hannah", "Divi", "Jodie", "Ivan" };
 	public string name { get; set; }
 	private int difficulty = 100;
+	private int money;
 
-	private void setName() {
+	private void SetName() {
 		this.name = aiNames[Random.Shared.Next(aiNames.Length)];
 	}
 
-	public AIPlayers(int multiplier) {
-		setName();
+	private int GetMoney() {
+		return this.money;
+	}
+
+	public void SetMoney(int money) {
+		this.money = money;
+	}
+
+	public AIPlayers(int multiplier) { 
+		SetName();
 		// just multiply the difficulty by the multiplier, which will be set by the users when they set the ai difficulties
 		difficulty *= multiplier;
 	}
--- Cpsc370Final/NormalPlayer.cs
+++ Cpsc370Final/NormalPlayer.cs
@@ -1,5 +1,15 @@
 namespace Cpsc370Final;
 
 public class NormalPlayer {
-	
+	private int money;
+
+
+	public int GetMoney() {
+		return money;
+	}
+
+	public void SetMoney(int money) {
+		this.money = money;
+	}
+
 }
