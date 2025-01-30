91f3181b5e844a894b808f2f13df76b679bc953f
Iván Dombiak <idombiak@gmail.com>
2025-01-29 20:04:50 -0800

    Reformatted and refactored a bit of the WOF class alongslide game loop, to better follow SRP and encapsulation.

--- Cpsc370Final/WheelOfFortune.cs
+++ Cpsc370Final/WheelOfFortune.cs
@@ -2,50 +2,7 @@ namespace Cpsc370Final;
 
 class WheelOfFortune
 {
-    private static readonly Dictionary<string, List<string>> CategoryPhrases = new Dictionary<string, List<string>>
-    {
-        {
-            "Social media apps",
-            new List<string>
-            {
-                "Facebook", "Instagram", "Snapchat", "TikTok", "Twitter", "LinkedIn", "Pinterest", "Reddit", "WhatsApp"
-            }
-        },
-        {
-            "Fast food restaurants",
-            new List<string>
-            {
-                "McDonald's", "Burger King", "KFC", "Taco Bell", "Wendy's", "Subway", "Chick-fil-A", "Domino's Pizza",
-                "Popeyes"
-            }
-        },
-        {
-            "Reality TV shows",
-            new List<string>
-            {
-                "Survivor", "The Bachelor", "Keeping Up with the Kardashians", "Big Brother", "The Amazing Race",
-                "The Voice", "Love Island", "Hell's Kitchen", "Shark Tank"
-            }
-        },
-        {
-            "Celebrities",
-            new List<string>
-            {
-                "Taylor Swift", "Leonardo DiCaprio", "Kim Kardashian", "Beyoncé", "Dwayne Johnson", "Rihanna",
-                "Tom Holland", "Ariana Grande", "Chris Hemsworth"
-            }
-        },
-        {
-            "Tongue twisters",
-            new List<string>
-            {
-                "Sally sells seashells by the seashore", "Peter Piper picked a peck of pickled peppers",
-                "How much wood would a woodchuck chuck", "Betty Botter bought some butter", "Fuzzy Wuzzy was a bear",
-                "Six slippery snails slid slowly seaward"
-            }
-        }
-    };
-
+    GameLoop gameLoop = new GameLoop();
     public static void StartGame()
     {
         DelayPrint("===============================================");
@@ -77,8 +34,8 @@ class WheelOfFortune
 
         DisplayInstructions();
 
-        string category = ChooseRandomCategory();
-        string phrase = ChooseRandomPhrase(category);
+        string category = GameLoop.ChooseRandomCategory();
+        string phrase = GameLoop.ChooseRandomPhrase(category);
 
         DelayPrint($"The chosen category is: {category}");
         DelayPrint($"Your phrase to guess is: {phrase}");
@@ -106,20 +63,6 @@ class WheelOfFortune
         DelayPrint("Press any key to start the game!");
         Console.ReadKey();
         Console.WriteLine();
-    }
-
-    static string ChooseRandomCategory()
-    {
-        Random random = new Random();
-        var categoryList = new List<string>(CategoryPhrases.Keys);
-        return categoryList[random.Next(categoryList.Count)];
-    }
-
-    static string ChooseRandomPhrase(string category)
-    {
-        Random random = new Random();
-        List<string> phrases = CategoryPhrases[category];
-        return phrases[random.Next(phrases.Count)];
     }
 
     public static void DelayPrint(string message)
