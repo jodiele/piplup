4413492d7de55fa1f8dfa187281df56d1a7b6d35
Iván Dombiak <idombiak@gmail.com>
2025-01-30 19:14:53 -0800

    Added some unit tests

--- Cpsc370Final.Tests/UnitTest1.cs
+++ Cpsc370Final.Tests/UnitTest1.cs
@@ -3,7 +3,25 @@ namespace Cpsc370Final.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CheckUserMoneyAmount()
     {
+        NormalPlayer player = new NormalPlayer();
+        Assert.Equal(2000, player.GetMoney());
+    }
+    
+    [Fact]
+    public void CheckUserMoneyAmountAfterAdding()
+    {
+        NormalPlayer player = new NormalPlayer();
+        player.AddMoney(1000);
+        Assert.Equal(3000, player.GetMoney());
+    }
+    
+    [Fact]
+    public void CheckAIMoneyAmount()
+    {
+        AIPlayers ai = new AIPlayers(1);
+        ai.SetMoney(2000);
+        Assert.Equal(2000, ai.GetMoney());
     }
 }
